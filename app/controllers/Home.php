4b1b52fb5e8179c219f43b8590e2097781e7b96d
Mohammad Rangga Irghivya <mohammadranggairghivya@gmail.com>
2025-02-02 20:47:56 +0700

    logged out users can still enter other pages has been fixed

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,8 +8,10 @@
 
         public function authentication() {
             if(!session_id()) session_start();
+
             $user = $this->model('Auth_model')->findUserByEmail($_POST['email']);
             $user['pass'] = password_hash($user['pass'], PASSWORD_DEFAULT);
+
             if(password_verify($_POST['pass'], $user['pass'])) {
                 $_SESSION['id_petugas'] = $user['id_petugas'];
                 header('Location: ' . BASE_URL . '/admin/');
@@ -22,8 +24,19 @@
         }
 
         public function logout() {
-            session_unset();
+            if(!session_id()) session_start();
+            
+            $_SESSION = [];
+
+            if(ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                    $params["path"], $params["domain"],
+                    $params["secure"], $params["httponly"]
+                );    
+            }
             session_destroy();
+
             header('Location: ' . BASE_URL . '/home');
             exit;
         }
--- app/core/Auth.php
+++ app/core/Auth.php
@@ -2,6 +2,9 @@
     if(!session_id()) session_start();
     
     if(!isset($_SESSION['id_petugas'])) {
+        header("Cache-Control: no-cache, must-revalidate, no-store, max-age=0");
+        header('Pragma: no-cache');
+
         header('Location: http://localhost/library-management-system/public/home/login');
         exit;
     }
